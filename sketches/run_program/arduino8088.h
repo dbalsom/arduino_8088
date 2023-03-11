04f5a2fd39647a9fc0fd33624e9d324c7a164f4c
dbalsom <dbalsom@gmail.com>
2023-03-11 13:36:46 -0500

    add RQ pin

--- sketches/run_program/arduino8088.h
+++ sketches/run_program/arduino8088.h
@@ -329,6 +329,9 @@ const int RESET_PIN = 5;
 #define NMI_PIN 13
 #define WRITE_NMI_PIN(x) (PORTB |= ((x) << 7))
 
+// -------------------------- CPU Output pins ---------------------------------
+#define RQ_PIN 3
+
 // --------------------------8288 Control Inputs ------------------------------
 #define AEN_PIN 54
 #define READ_AEN_PIN ((PINF & 0x01) != 0)
@@ -404,7 +407,7 @@ const int OUTPUT_PINS[] = {
 
 // All input pins, used to set pin direction on setup
 const int INPUT_PINS[] = {
-  8,9,10,11,14,15,16,
+  3,8,9,10,11,14,15,16,
   23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,
   43,44,45,46,47,48,49,50,51,52,53,
 };
